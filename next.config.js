ecb736655f7fef2f49d13f9ceebae185fc15787e
bls220 <bls220@vt.edu>
2020-12-06 21:22:09 -0500

    Wrapped links in NextJs Link, so that base_path could be applied.

---
+++ next.config.js
@@ -0,0 +1,5 @@
+
+module.exports = {
+  basePath: process.env.BASE_PATH || "",
+  assetPrefix: process.env.BASE_PATH || "",
+};
--- pages/_app.js
+++ pages/_app.js
@@ -1,44 +1,89 @@
-import '../styles/customTheme.scss'
-import { Container, Row, Col, Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap'
+import "../styles/customTheme.scss";
+import Link from "next/link";
+import {
+  Container,
+  Row,
+  Col,
+  Navbar,
+  Nav,
+  NavDropdown,
+  Form,
+  FormControl,
+  Button,
+} from "react-bootstrap";
 
-function MyApp( { Component, pageProps } ) {
-    return (
-      <>
-        <Navbar variant='dark' expand="lg">
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Navbar variant="dark" expand="lg">
         <Container className="container-fluid">
-          <Navbar.Brand href="/">What's Your Ghost?</Navbar.Brand>
+          <Link href="/" passHref>
+            <Navbar.Brand>What's Your Ghost?</Navbar.Brand>
+          </Link>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
-              <Nav.Link href="/">Home</Nav.Link>
-              <Nav.Link href="#link">Photos</Nav.Link>
+              <Link href="/" passHref>
+                <Nav.Link>Home</Nav.Link>
+              </Link>
+              <Link href="#link" passHref>
+                <Nav.Link>Photos</Nav.Link>
+              </Link>
               <NavDropdown title="Meet The Ghosts" id="basic-nav-dropdown">
-                <NavDropdown.Item href="/ghost/spirit">Spirit</NavDropdown.Item>
-                <NavDropdown.Item href="/ghost/wraith">Wraith</NavDropdown.Item>
-                <NavDropdown.Item href="/ghost/phantom">Phantom</NavDropdown.Item>
-                <NavDropdown.Item href="/ghost/poltergeist">Poltergeist</NavDropdown.Item>
-                <NavDropdown.Item href="/ghost/banshee">Banshee</NavDropdown.Item>
-                <NavDropdown.Item href="/ghost/jinn">Jinn</NavDropdown.Item>
-                <NavDropdown.Item href="/ghost/mare">Mare</NavDropdown.Item>
-                <NavDropdown.Item href="/ghost/revenant">Revenant</NavDropdown.Item>
-                <NavDropdown.Item href="/ghost/shade">Shade</NavDropdown.Item>
-                <NavDropdown.Item href="/ghost/demon">Demon</NavDropdown.Item>
-                <NavDropdown.Item href="/ghost/yurei">Yurei</NavDropdown.Item>
-                <NavDropdown.Item href="/ghost/oni">Oni</NavDropdown.Item>
+                <Link href="/ghost/spirit" passHref>
+                  <NavDropdown.Item>Spirit</NavDropdown.Item>
+                </Link>
+                <Link href="/ghost/wraith" passHref>
+                  <NavDropdown.Item>Wraith</NavDropdown.Item>
+                </Link>
+                <Link href="/ghost/phantom" passHref>
+                  <NavDropdown.Item>Phantom</NavDropdown.Item>
+                </Link>
+                <Link href="/ghost/poltergeist" passHref>
+                  <NavDropdown.Item>Poltergeist</NavDropdown.Item>
+                </Link>
+                <Link href="/ghost/banshee" passHref>
+                  <NavDropdown.Item>Banshee</NavDropdown.Item>
+                </Link>
+                <Link href="/ghost/jinn" passHref>
+                  <NavDropdown.Item>Jinn</NavDropdown.Item>
+                </Link>
+                <Link href="/ghost/mare" passHref>
+                  <NavDropdown.Item>Mare</NavDropdown.Item>
+                </Link>
+                <Link href="/ghost/revenant" passHref>
+                  <NavDropdown.Item>Revenant</NavDropdown.Item>
+                </Link>
+                <Link href="/ghost/shade" passHref>
+                  <NavDropdown.Item>Shade</NavDropdown.Item>
+                </Link>
+                <Link href="/ghost/demon" passHref>
+                  <NavDropdown.Item>Demon</NavDropdown.Item>
+                </Link>
+                <Link href="/ghost/yurei" passHref>
+                  <NavDropdown.Item>Yurei</NavDropdown.Item>
+                </Link>
+                <Link href="/ghost/oni" passHref>
+                  <NavDropdown.Item>Oni</NavDropdown.Item>
+                </Link>
               </NavDropdown>
             </Nav>
             <Form inline>
-              <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+              <FormControl
+                type="text"
+                placeholder="Search"
+                className="mr-sm-2"
+              />
               <Button variant="outline-primary">Search</Button>
             </Form>
           </Navbar.Collapse>
         </Container>
       </Navbar>
 
-      <br/>
-      <Component { ...pageProps } />
-      </>
-    )
+      <br />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
